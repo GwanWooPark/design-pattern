b4b20efc15886d69ffe5cc12eef6bea924f66886
GwanWooPark <pgw4712@icloud.com>
2022-05-24 22:34:20 +0900

    싱클톤을 깨는 방법

--- src/main/java/com/study/designpattern/singleton/App.java
+++ src/main/java/com/study/designpattern/singleton/App.java
@@ -1,13 +1,34 @@
 package com.study.designpattern.singleton;
 
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        Settings settings1 = Settings.getInstance();
-        Settings settings2 = Settings.getInstance();
+        Settings settings = Settings.getInstance();
+        Settings settings1 = null;
+
+        // 직렬화 & 역직렬화 : 저장할 때 직렬화, 읽어들일 때는 역직렬화
+//                         객체를 파일로 저장해 놓았다가 다시 로딩할 수 있음
+//                         역직렬화를 하면 반드시 생성자를 사용해서 인스턴스를 만든다
+        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+            out.writeObject(settings);
+        }
+
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))){
+            settings1 = (Settings) in.readObject();
+        }
 
 
-        System.out.println(settings1 != settings2);
+
+        // 리플렉션: Singleton을 깨는 첫 번째 방법, 대응 불가
+//        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+//        constructor.setAccessible(true);
+//        Settings settings = constructor.newInstance();
+
+        System.out.println(settings == settings1);
     }
 }
--- src/main/java/com/study/designpattern/singleton/Settings.java
+++ src/main/java/com/study/designpattern/singleton/Settings.java
@@ -1,6 +1,8 @@
 package com.study.designpattern.singleton;
 
-public class Settings {
+import java.io.Serializable;
+
+public class Settings implements Serializable {
 
     // 1. private로 생성자를 생성하면 클래스가 다르면 생성자를 사용할 수 없다.
     private Settings() {
@@ -21,17 +23,24 @@ public class Settings {
 
     /**
      * 4. 해결 방법
-     *   1) synchronized : 한번에 하나의 쓰레드만 들어오게 하는 것 (단, getInstance를 실행할 때마다 동기화 처리 작업 때문에 성능의 저하가 있음)
-     *   2) 이른 초기화(eager initialization): 미리 만드는 것(초기화 비용이 크지 않을 때)
-     *          private static final Settings INSTANCE = new Settings();
-     *          장점: Thread Safe
-     *          단점: instance를 만드는데 비용을 많이 소모 했지만, 사용하지 않을 때
-     *   3) double checked locking(효율적인 동기화 블럭): 1.5 이상에서 사용됨
-     *          private static volatile Settings instance;
-     *   4) static inner(권장되는 방법 중 1개):
+     * 1) synchronized : 한번에 하나의 쓰레드만 들어오게 하는 것 (단, getInstance를 실행할 때마다 동기화 처리 작업 때문에 성능의 저하가 있음)
+     * 2) 이른 초기화(eager initialization): 미리 만드는 것(초기화 비용이 크지 않을 때)
+     * private static final Settings INSTANCE = new Settings();
+     * 장점: Thread Safe
+     * 단점: instance를 만드는데 비용을 많이 소모 했지만, 사용하지 않을 때
+     * 3) double checked locking(효율적인 동기화 블럭): 1.5 이상에서 사용됨
+     * private static volatile Settings instance;
+     * 4) static inner(권장되는 방법 중 1개):
+     *
      * @return
      */
     public static Settings getInstance() {
         return SettingHolder.INSTANCE;
     }
+
+    // 역직렬화 대응 방안
+    protected Object readResolve() {
+        return getInstance();
+    }
+
 }
