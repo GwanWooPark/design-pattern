4ab52182bace1377b9b1b8da17da12ddc4a74b54
GwanWooPark <pgw4712@icloud.com>
2022-06-16 17:52:23 +0900

    after interpreter pattern

---
+++ src/main/java/com/study/designpattern/_03_behavioral_patterns/_03_interpreter/_02_after/PostfixExpression.java
@@ -0,0 +1,8 @@
+package com.study.designpattern._03_behavioral_patterns._03_interpreter._02_after;
+
+import java.util.Map;
+
+public interface PostfixExpression {
+
+    int interpret(Map<Character, Integer> context);
+}
