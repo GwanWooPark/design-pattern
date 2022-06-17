1866bdb42d5389247bce0dae2b47a7c281db068f
GwanWooPark <pgw4712@icloud.com>
2022-06-17 18:58:58 +0900

    before memento pattern

---
+++ src/main/java/com/study/designpattern/_03_behavioral_patterns/_06_memento/_01_before/Client.java
@@ -0,0 +1,24 @@
+package com.study.designpattern._03_behavioral_patterns._06_memento._01_before;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.setBlueTeamScore(10);
+        game.setRedTeamScore(20);
+
+        int redTeamScore = game.getRedTeamScore();
+        int blueTeamScore = game.getBlueTeamScore();
+
+        /*
+            encapsulation이 깨진 상태
+            Client가 게임의 내부 정보가 뭐가 있는지 일일이 알고 있어야 함.
+             --> 이 자체가 문제(내부 정보 의존하고 있기 때문에 무언가가 바뀌게 되면 Client의 코드가 변경이 된다)
+
+         */
+        Game restoredGame = new Game();
+        restoredGame.setRedTeamScore(redTeamScore);
+        restoredGame.setBlueTeamScore(blueTeamScore);
+    }
+}
---
+++ src/main/java/com/study/designpattern/_03_behavioral_patterns/_06_memento/_01_before/Game.java
@@ -0,0 +1,23 @@
+package com.study.designpattern._03_behavioral_patterns._06_memento._01_before;
+
+public class Game {
+
+    private int redTeamScore;
+    private int blueTeamScore;
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+
+    public void setRedTeamScore(int redTeamScore) {
+        this.redTeamScore = redTeamScore;
+    }
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+
+    public void setBlueTeamScore(int blueTeamScore) {
+        this.blueTeamScore = blueTeamScore;
+    }
+}
