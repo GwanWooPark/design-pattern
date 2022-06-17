a5001a15e0334546fd274198339df94904bf97c6
GwanWooPark <pgw4712@icloud.com>
2022-06-17 17:04:30 +0900

    before mediator pattern

---
+++ src/main/java/com/study/designpattern/_03_behavioral_patterns/_05_mediator/_01_before/CleaningService.java
@@ -0,0 +1,17 @@
+package com.study.designpattern._03_behavioral_patterns._05_mediator._01_before;
+
+public class CleaningService {
+
+
+    public void clean(Gym gym) {
+        System.out.println("clean " + gym);
+    }
+
+    public void getTower(Guest guest, int numberOfTower) {
+        System.out.println(numberOfTower + " towers to " + guest);
+    }
+
+    public void clean(Restaurant restaurant) {
+        System.out.println("clean " + restaurant);
+    }
+}
