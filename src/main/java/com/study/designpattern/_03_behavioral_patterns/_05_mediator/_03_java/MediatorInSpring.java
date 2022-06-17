dcfdb2e8c6389c74ab8110a0a15db1de7588394f
GwanWooPark <pgw4712@icloud.com>
2022-06-17 18:10:35 +0900

    mediator pattern in Java

---
+++ src/main/java/com/study/designpattern/_03_behavioral_patterns/_05_mediator/_03_java/MediatorInSpring.java
@@ -0,0 +1,13 @@
+package com.study.designpattern._03_behavioral_patterns._05_mediator._03_java;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MediatorInSpring {
+
+    public static void main(String[] args) {
+        /**
+         * DispatcherServlet이 Spring에서 대표적인 Mediator Pattern을 적용한 사례이다.
+         */
+        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+    }
+}
