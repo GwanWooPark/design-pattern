086468f644a2b38e261ea94eb55e5c02f0d3def1
GwanWooPark <pgw4712@icloud.com>
2022-06-21 17:52:19 +0900

    before template method pattern

---
+++ src/main/java/com/study/designpattern/_03_behavioral_patterns/_10_template/_01_before/FileProcessor.java
@@ -0,0 +1,29 @@
+package com.study.designpattern._03_behavioral_patterns._10_template._01_before;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileProcessor {
+
+    private String path;
+
+    public FileProcessor(String path) {
+        this.path = path;
+    }
+
+    public int process() {
+
+        // try() 안에 소스를 작성하면 finally를 통해서 close를 해주지 않아도 된다.(auto call)
+        try(BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            int result = 0;
+            String line = null;
+            while((line = reader.readLine()) != null) {
+                result += Integer.parseInt(line);
+            }
+            return result;
+        } catch (IOException e) {
+            throw new IllegalArgumentException(path + "에 해당하는 파일이 없습니다.", e);
+        }
+    }
+}
